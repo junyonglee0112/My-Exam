bc0c44c49a7c2e2965cd7568bc3ed6c567120282
junyonglee0112 <junyonglee0112@gmail.com>
2025-07-21 15:12:22 +0900

    Create loop01.c

---
+++ C/Basic/loop01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	//1. 초기값.
+	int i = 100;
+
+	while (i >= 0)
+	{
+		printf("% d\n", i);
+		//i = i - 1;
+		// i -= 1;
+		 i--;
+
+	}
+
+	return 0;
